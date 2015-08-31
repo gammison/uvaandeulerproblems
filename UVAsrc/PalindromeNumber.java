9be8d8670dc6313b037912fa0035dff9c443bdd9
gammison <garrisonwgrogan@gmail.com>
2015-08-31 17:46:22 -0500

    palindrome number solution, too slow

--- UVAsrc/PalindromeNumber.java
+++ UVAsrc/PalindromeNumber.java
@@ -1,85 +1,105 @@
-// problem id 12050
+// problem id 120L50L
 import java.util.*;
 import static java.lang.System.*;
 
-public class PalindromeNumbers {
-	public static void main(String[] args)
-	{
-		Scanner in = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PalindromeNumber {
+	public static void main(String[] args) throws IOException
+	{	InputStreamReader input = new InputStreamReader(new FileInputStream(("input.in")));
+	
+		BufferedReader inputty = new BufferedReader(input);
 		
-		while(in.hasNext())
+		
+		do 
 		{
-			
-			int index = in.nextInt();
-			if(index == 0)
+			String token = inputty.readLine();
+			long index = Long.parseLong(token); 
+			if(index == 0L)
 			{	break;
 			
 			}
 			
-			int lowerbound = 0;
-			int pl10 = 0;
-			int startn = 0;
-			int startcount = 0;
-			int stat = pslessn(pl10);
+			long pl10L = 0L;
+			long startn = 0L;
+			long startcount = 0L;
+			long stat = pslessn(pl10L);
+			
 			while(index > stat)
 			{	
-				pl10++;
-				stat = pslessn(pl10);
+				pl10L++;
+				stat = pslessn(pl10L);
 				if(index<stat){
-					pl10--; startn = (int)Math.pow(10, pl10); startcount = stat; break;
+					pl10L--;
+					startn = (int)Math.pow(10L, pl10L);
+					startcount = pslessn(pl10L);
+					break;
 				}
 				
 			}
-			 if(index < 10){
-					startn = 0; startcount = 1;
-				}
-			
-			while(startcount != index)
+			if(index < 10L)
 			{
-				if(startn == revdig(startn))
-				{	
+					startn = 1; startcount = 1;
+			}
+			
+			long upperbound = (int) Math.pow(10L, pl10L+1);
+			
+			/*while(startcount != index && startn<upperbound)
+			{	
+				if(ispalindromic(startn))
+				{	startcount++;
+					//out.println(startcount + " " + index);
+				}
+				if(startcount==index)
+					break;
+				startn++;
+			}*/
+			
+			for(long i = startn; i<upperbound; i++)
+			{
+				if(ispalindromic(i))
+				{
 					startcount++;
 				}
-				startn++;
+				if(startcount == index)
+					break;
 			}
+			
 			out.println(startn);
 			
 			
-		}
+		}while(inputty.ready());
 	}
 	
-	public static int lowerbound(int index)
+	public static long lowerbound(long index)
 	{
-		int pw10 = 0;
-		while(index/10 > 0)
-		{
-			pw10++;
-			index/=10;
-		}
-		return pslessn(pw10);
+		long pw10L = (long) Math.ceil(Math.log10(index+1)); out.println(pw10L);
+		return pslessn(pw10L);
 	}
-	public static int pslessn(int pw10)
+	public static long pslessn(long pw10L)
 	{
-		if(pw10%2 == 0)
+		if(pw10L%2 == 0L)
 		{
-			return (int) (2 * (Math.pow(10, pw10/2)-1));
+			return (int) (2 * (Math.pow(10L, pw10L/2)-1));
 		}
 		else
-			return (int)(11 * Math.pow(10, (pw10-1)/2) - 2);
+			return (int)(11 * Math.pow(10L, (pw10L-1)/2) - 2);
 	}
 	
-	public static int revdig(int num)
+
+	public static boolean ispalindromic(long i)
 	{
-		int tempn = 0;
-		while(num>0)
-		{
-			tempn *= 10;
-			tempn+= num%10;
-			num/=10;
-		}
+		   long len = (int) Math.ceil(Math.log10(i+1));
+		    for (long n = 0; n < len / 2; n++)
+		    {   	if ((i / (int) Math.pow(10, n)) % 10 != (i / (int) Math.pow(10, len - n - 1)) % 10)
+		    		{  	 
+		            	return false;
+		    		}
+		    }
+		    return true;
 		
-		
-		
-		return tempn;
 	}
 }
