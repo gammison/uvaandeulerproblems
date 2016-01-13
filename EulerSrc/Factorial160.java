3b2e3bd46134789021ee725bea448f419060b648
gammison <garrisonwgrogan@gmail.com>
2016-01-13 10:42:53 -0600

    Update Factorial160.java

--- EulerSrc/Factorial160.java
+++ EulerSrc/Factorial160.java
@@ -7,11 +7,12 @@
  	public static void main(String[] args) throws IOException
  	{
  		Scanner in = new Scanner(System.in);
- 		
+ 		String dat = "";
  		while(in.hasNextLine())
  		{
  			long n = in.nextLong();
- 			if(n == 0) break;
+ 			if(n == 0)
+ 				break;
  			//find primes in each less n and start count
  			//out.println(pfacs(n));
  			ArrayList<Integer> factorial = pfacs(n);
@@ -39,9 +40,35 @@
  	 				}
  				}
  				
- 			}out.println(n+"! = "+factorial.toString().replaceAll("[\\[\\]]|\\,0", "").replaceAll(",", "").replaceAll("0\\s", "").replaceAll("\\s0", ""));
+ 			}String n1 =""+ n;
+ 			while(n1.length()<3)
+ 			{
+ 				n1 = " "+n1;
+ 			}dat = ""+n1+"! =";
+ 			//out.print(n1+"! =");
+ 			int cnt = 1; String[] nums = factorial.toString().replaceAll("[\\[\\]]|\\,0", "").replaceAll(",", "").replaceAll("0\\s", "").replaceAll("\\s0", "").split(" ");
+ 			for(int x = 0; x<nums.length; x++)
+ 			{	while(nums[x].length()<3)
+ 					nums[x] =" "+nums[x];
+ 				if(x+1-(15*cnt) <0)
+ 					{	dat = dat +nums[x];
+ 						//out.print(nums[x]+"");
+ 					}
+ 				else if(x+1-(15*cnt)<=0){
+ 					dat = dat +nums[x];out.print(nums[x]);
+ 				}
+ 					
+ 				else if(x+1-(15*cnt) == 1)
+ 				{	cnt++;
+ 					//out.print("\n      "+nums[x]);
+ 					dat = dat +"\n     "+nums[x];
+ 				}
+ 			}
+ 			out.println(dat);
+ 			
  			
  		}
+ 		
  	}
  	
  	public static ArrayList<Integer> pfacs(long n)
