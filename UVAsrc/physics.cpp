bd648cef4542e2d63e9e688928802b8c2b0f4e04
Garrison Grogan <garrisonwgrogan@gmail.com>
2016-03-12 18:13:01 -0600

    solved 10071
    solved 10071

---
+++ UVAsrc/physics.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    long x,y;
+    //std::cout<<"fuck you"<<std::endl;
+    while(std::cin>>x>>y){
+        std::cout<<x*y*2<<std::endl;
+    }
+    return 0;
+}
